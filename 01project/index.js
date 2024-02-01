9b57b35ed61ea4581ecbc99fcbe09c2abfca9a84
Ajmal Hossain <ajmalhossainopu9@gmail.com>
2024-02-01 09:03:52 +0600

    updated

--- 01project/index.js
+++ 01project/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectMongoDB = require("./connection");
 const logRequest = require("./middlewares/requestLogs");
 const userRouter = require("./routes/User");
+const path = require("path");
 
 const app = express();
 const PORT = 8000;
@@ -11,9 +12,17 @@ connectMongoDB("mongodb://127.0.0.1:27017/01project").then(
   console.log("MongoDB Connected!")
 );
 
+//Set Templeting Engine
+express.set("view engine", "ejs");
+express.set("views", path.resolve("./views"));
+
 // Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(logRequest);
+
+app.get("/test", (req, res) => {
+  return res.render("home");
+});
 
 //Routes
 app.use("/api/users", userRouter);
--- 02project/index.js
+++ 02project/index.js
@@ -1,39 +1,47 @@
-const express = require('express');
-const connectMongoDB = require('./connection');
-const urlRouter = require('./routes/Url');
-const URL = require('./models/Url');
+const express = require("express");
+const connectMongoDB = require("./connection");
+const urlRouter = require("./routes/Url");
+const URL = require("./models/Url");
+const path = require("path");
 
 const app = express();
 const PORT = 8001;
 
-connectMongoDB('mongodb://127.0.0.1:27017/02project').then(() => {
-    console.log('MongoDB Connected!');
+connectMongoDB("mongodb://127.0.0.1:27017/02project").then(() => {
+  console.log("MongoDB Connected!");
 });
+
+//Set Templeting Engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
 //Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get("/test", (req, res) => {
+  return res.render("home");
+});
+
 //Routes
-app.use('/url', urlRouter);
+app.use("/url", urlRouter);
 
-app.get('/:shortId', async (req, res) => {
-
-    const shortId = req.params.shortId;
-    const result = await URL.findOneAndUpdate(
-        {
-            shortId
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const result = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitiHistory: {
+          timestamp: Date.now(),
         },
-        {
-            $push: {
-                visitiHistory: {
-                    timestamp: Date.now()
-                }
-            }
-        }
-    );
+      },
+    }
+  );
 
-    res.redirect(result.redirectUrl);
+  res.redirect(result.redirectUrl);
 });
 
 //Run App
